e84858fb13628e648cb20b503b44700f0bd9df96
neha doodi <nehadoodipoonia@gmail.com>
2020-02-14 10:39:27 -0800

    try to use api key

--- src/main.js
+++ src/main.js
@@ -3,3 +3,26 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import { Doctors } from "./../src/doctors.js";
+
+$(document).ready(function() {
+  $("#getDoctors").click(function(event) {
+    event.preventDefault();
+
+    (async () => {
+      let doctor = new Doctors();
+      const response = await doctor.getDoctorByCondition();
+      this.getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
+        for (let i = 0; i < response.length; i++) {
+          console.log(response.data[i].profile.first_name);
+          $("#results").append(`<p>${response.data[i].profile.first_name}</p>`);
+        }
+      } else {
+        console.log("There was an error handling your request");
+      }
+    }
+  });
+});
