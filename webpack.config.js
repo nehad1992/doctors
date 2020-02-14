6091da6983bdfafeccb363ec4d197012dd974cbe
neha doodi <nehadoodipoonia@gmail.com>
2020-02-14 09:12:18 -0800

    modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,33 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); 
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
-  devtool: 'eval-source-map',
-  devServer: {               
-    contentBase: './dist'    
-        },
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
   plugins: [
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Triangle Tracker',
-      template: './src/index.html',
-      inject: 'body'
-    })
+      title: "doctors",
+      template: "./src/index.html",
+      inject: "body"
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.js$/,
