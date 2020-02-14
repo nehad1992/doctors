60e6903784e5c83429757f290fc408a359d440dd
neha doodi <nehadoodipoonia@gmail.com>
2020-02-14 09:03:22 -0800

    add form to html

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import $ from 'jquery';
-import 'bootstrap' ; 
-import 'bootstrap/dist/css/bootstrap.min.css' ; 
-import './styles.css' ;
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import { Doctors } from "./../src/doctors.js";
