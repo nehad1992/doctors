8196cdf3053dd5b6194cf0ab276be87152b0d228
neha doodi <nehadoodipoonia@gmail.com>
2020-02-23 22:25:21 -0800

    add API to find doctor by name

--- src/doctorService.js
+++ src/doctorService.js
@@ -1,7 +1,8 @@
 // Business Interface
 export class DoctorService {
+  // Business Logic to search doctors by condition
   async getDoctorByCondition(condition) {
-//     try {
+    try {
       let response = await fetch(
         `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=37.773%2C-122.413%2C100&skip=0&limit=10&user_key=${process.env.API_KEY}`
       );
@@ -13,8 +14,25 @@ export class DoctorService {
       }
 
       return jsonifiedResponse;
-//     } catch {
-//       return false;
-//     }
+    } catch (error) {
+      alert("Invalid Entry!");
+    }
+  }
+  //  Business Logic to search doctors by Name
+  async getDoctorByName(name) {
+    try {
+      let response = await fetch(
+        `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${name}&location=47.60623%2C-122.3321%2C100&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key=${process.env.API_KEY}`
+      );
+      let jsonifiedResponse;
+      if (response != null && response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error) {
+      alert("Invalid Entry!");
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import "./styles.css";
 import { DoctorService } from "./../src/doctorService.js";
 
 $(document).ready(function() {
+  // User Interface to find doctors bu condition
   $("#getDoctors").click(function(event) {
     event.preventDefault();
     $(".showDoctors").text("");
@@ -38,6 +39,48 @@ $(document).ready(function() {
         }
       } else {
         $(".showDoctors").text("Invalid entry");
+      }
+    }
+  });
+
+  // User Interface to find doctors by name
+  $("#findDoctor").click(function(event) {
+    event.preventDefault();
+    $(".showDoctorByName").text("");
+    var doctorName = $("#doctorName").val();
+
+    (async () => {
+      let doctorService = new DoctorService();
+      const response = await doctorService.getDoctorByName(doctorName);
+      getElements(response);
+    })();
+
+    $("#showDoctorByName").val("");
+
+    function getElements(response) {
+      var doctorNameArray = [];
+      if (response && response.data && response.data.length > 0) {
+        doctorNameArray = response.data;
+        for (var i = 0; i < doctorNameArray.length; i++) {
+          var doctorNameList = doctorNameArray[i];
+          var indDoctorByName = doctorNameList.profile;
+
+          let firstNameOfDoctor = indDoctorByName.first_name;
+          let middleNameOfDoctor = indDoctorByName.middle_name;
+          let lastNameofDoctor = indDoctorByName.last_name;
+
+          $(".showDoctorByName").append(
+            "<li>" +
+              firstNameOfDoctor +
+              " " +
+              middleNameOfDoctor +
+              " " +
+              lastNameofDoctor +
+              "</li>"
+          );
+        }
+      } else {
+        $(".showDoctorByName").text("Invalid entry");
       }
     }
   });
